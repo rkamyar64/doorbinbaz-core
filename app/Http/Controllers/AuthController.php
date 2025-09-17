01985bc0ee50614d687b1dd6b9a4c39e17fd0314
rkamyar64 <rezakam@gmail.com>
2025-09-17 08:02:19 +0330

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+
     /**
+     * reza
      * @OA\Post(
      *     path="/api/register",
      *     summary="Register a new user",
